621ab7be89ca2c5a0d969a5b3c8fc850619c30a0
nebulabox <nebulabox@gmail.com>
2023-01-30 21:18:48 +0800

    no message

--- gen_linux_route.sh
+++ gen_linux_route.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
+# ip -6 route add 2606:4700:4700::/48 via fe80::1 dev eth0
 output="./cn.lin.up.sh"
 rm $output
 GW="\$1"
 GW6="\$2"
+DEV="\$3"
 CNIPFILE="./china_cidr4.txt"
 CN6IPFILE="./china_cidr6.txt"
 
 cat > $output << EOF
 #!/usr/bin/env zsh
+if [ "$#" -ne 3 ]; then
+    echo "gen_linux_route.sh ipv4gw ipv6gw devname"
+    exit 1
+fi
 # some special ips
 # mars
-sudo ip route add 150.230.40.135/32 via $GW
-sudo ip route add 2603:c024:c004:5900::/64 via $GW6
+sudo ip route add 150.230.40.135/32 via $GW dev $DEV
+sudo ip route add 2603:c024:c004:5900::/64 via $GW6 dev $DEV
 # ac
-sudo ip route add 192.9.138.146/32 via $GW
+sudo ip route add 192.9.138.146/32 via $GW dev $DEV
 # venus
-sudo ip route add 155.248.177.72/32 via $GW
-sudo ip route add 2603:c021:800c:c9ee::/64 via $GW6
+sudo ip route add 155.248.177.72/32 via $GW dev $DEV
+sudo ip route add 2603:c021:800c:c9ee::/64 via $GW6 dev $DEV
 # sun
-sudo ip route add 150.158.164.110/32 via $GW
+sudo ip route add 150.158.164.110/32 via $GW dev $DEV
 #####
 EOF
 for A in `cat $CNIPFILE`; do
-   echo "sudo ip route add $A via $GW"
-   echo "sudo ip route add $A via $GW" >> $output
+   echo "sudo ip route add $A via $GW dev $DEV"
+   echo "sudo ip route add $A via $GW dev $DEV" >> $output
 done
 for A in `cat $CN6IPFILE`; do
-   echo "sudo ip route add $A via $GW6"
-   echo "sudo ip route add $A via $GW6" >> $output
+   echo "sudo ip route add $A via $GW6 dev $DEV"
+   echo "sudo ip route add $A via $GW6 dev $DEV" >> $output
 done
 
 output="./cn.lin.down.sh"
