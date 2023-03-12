5c8de40a120a40046d0a5923217abcfb22cda83d
nebulabox <nebulabox@gmail.com>
2023-03-12 19:02:45 +0800

    nn

--- gen_linux_route.sh
+++ gen_linux_route.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
-# sudo ip -6 r get default
-# sudo ip -6 route add 2606:4700:4700::/48 via fe80::aa5e:45ff:fe99:b6c0 dev ens32
-# sudo mtr 2606:4700:4700::1111
-# sudo ip -6 r delete 2606:4700:4700::/48
+# ip -6 r get default
+# ip -6 route add 2606:4700:4700::/48 via fe80::aa5e:45ff:fe99:b6c0 dev ens32
+# mtr 2606:4700:4700::1111
+# ip -6 r delete 2606:4700:4700::/48
 # OpenVPN ->: route_net_gateway="192.168.2.1"
 # OpenVPN ->: net_gateway_ipv6="fe80::9a00:74ff:feab:b353"
 output="./cn.lin.up.sh"
@@ -10,37 +10,42 @@ rm $output
 GW="\$route_net_gateway"
 # GW6="\$net_gateway_ipv6"
 CNIPFILE="./china_cidr4.txt"
+TUNDEV="\$1" # tun11
 # CN6IPFILE="./china_cidr6.txt"
 
 cat > $output << EOF
-#!/usr/bin/env zsh
+#!/bin/sh
+# default route
+# ip route add default via $TUNDEV
 # mars
-sudo ip route add 150.230.40.135/32 via $GW 
+ip route add 150.230.40.135/32 via $GW 
 # ac
-sudo ip route add 192.9.138.146/32 via $GW 
+ip route add 192.9.138.146/32 via $GW 
 # venus
-sudo ip route add 155.248.177.72/32 via $GW 
+ip route add 155.248.177.72/32 via $GW 
 # sun
-sudo ip route add 150.158.164.110/32 via $GW 
+ip route add 150.158.164.110/32 via $GW 
 #####
 EOF
 for A in `cat $CNIPFILE`; do
-   echo "sudo ip route add $A via $GW "
-   echo "sudo ip route add $A via $GW " >> $output
+   echo "ip route add $A via $GW "
+   echo "ip route add $A via $GW " >> $output
 done
 
 output="./cn.lin.down.sh"
 rm $output
 cat > $output << EOF
-#!/usr/bin/env zsh
-sudo ip route delete 192.9.138.146/32
-sudo ip route delete 150.230.40.135/32
-sudo ip route delete 155.248.177.72/32
-sudo ip route delete 150.158.164.110/32 
+#!/bin/sh
+ip route delete default
+ip route add default via $GW
+ip route delete 192.9.138.146/32
+ip route delete 150.230.40.135/32
+ip route delete 155.248.177.72/32
+ip route delete 150.158.164.110/32 
 EOF
 for A in `cat $CNIPFILE`; do
-   echo "sudo ip route delete $A"
-	echo "sudo ip route delete $A" >> $output
+   echo "ip route delete $A"
+	echo "ip route delete $A" >> $output
 done
 
 echo "end"
